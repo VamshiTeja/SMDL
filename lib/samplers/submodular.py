f875d8fd65000e5ac9c5210536afebf1d3414a81
Joseph KJ <josephkj20@gmail.com>
2018-11-03 11:18:30 +0530

    Updates

--- lib/samplers/submodular.py
+++ lib/samplers/submodular.py
@@ -21,11 +21,24 @@ class SubmodularSampler(Sampler):
         subset_indices = []     # Subset of indices. Keeping track to improve computational performance.
         final_score = []
 
+        class_mean = np.mean(self.penultimate_activations, axis=0)
+
         end = len(set) if dynamic_set_size else self.subset_size
 
         for i in range(0, end):
             now = time.time()
             scores = []
+
+            # Compute d_score for the whole subset. Then add the d_score of just the
+            # new item to compute the total d_score.
+            d_score = self._compute_d_score(list(subset_indices))
+
+            # Same logic for u_score
+            u_score = self._compute_u_score(list(subset_indices))
+
+            # Same logic for md_score
+            md_score = self.compute_md_score(list(subset_indices), class_mean)
+
             for iter, item in enumerate(set):
                 temp_subset = list(subset)
                 temp_subset_indices = list(subset_indices)
@@ -33,8 +46,14 @@ class SubmodularSampler(Sampler):
                 temp_subset.append(item)
                 temp_subset_indices.append(index_set[iter])
 
-                score = self._compute_score(temp_subset_indices, alpha_1, alpha_2)
+                d_score += self._compute_d_score(list([index_set[iter]]))
+                u_score += self._compute_u_score(list([index_set[iter]]))
+                md_score += self.compute_md_score((list([index_set[iter]])), class_mean)
+                r_score = self._compute_r_score(list(temp_subset_indices))
+
+                score = d_score + u_score + r_score + md_score
                 scores.append(score)
+
             best_item_index = np.argmax(scores)
             best_item = set[best_item_index]
 
@@ -55,36 +74,65 @@ class SubmodularSampler(Sampler):
         log(np.array(subset).shape)
         return np.array(subset)
 
-    def _compute_score(self, subset_indices, alpha_1, alpha_2):
+    def _compute_d_score(self, subset_indices, alpha=1.):
         """
-        Compute the score for the subset.
-        The score is a combination of:
-            1) Diversity Score: The new point should be distant from all the elements in the class.
-            2) Uncertainity Score: The point should make the model most confused.
-            3) Redundancy Score: The point should be distant from all the other elements in the subset.
-        :param subset:
-        :param alpha_1:
-        :param alpha_2:
-        :return: The score of the subset.
+        Computes the Diversity Score: The new point should be distant from all the elements in the class.
+        :param subset_indices:
+        :param alpha:
+        :return: d_score
         """
-        score = 0
+        d_score = 0
         for index in subset_indices:
-            # Diversity Score
             p_act = self.penultimate_activations[index]
             all_acts = self.penultimate_activations
-            d_score = np.sum(np.linalg.norm(all_acts - p_act, axis=1))
+            score = np.sum(np.linalg.norm(all_acts - p_act, axis=1))
+            d_score += alpha * score
 
-            # Uncertainity Score
+        return d_score
+
+    def _compute_u_score(self, subset_indices, alpha=1.):
+        """
+        Compute the Uncertainity Score: The point that makes the model most confused, should be preferred.
+        :param subset_indices:
+        :param alpha:
+        :return: u_score
+        """
+        u_score = 0
+        for index in subset_indices:
             f_acts = torch.tensor(self.final_activations[index])
             p_log_p = F.softmax(f_acts, dim=0) * F.log_softmax(f_acts, dim=0)
-            u_score = (-1.0 * p_log_p.sum()).numpy()
+            score = (-1.0 * p_log_p.sum()).numpy()
+            u_score += alpha * score
+        return u_score
 
-            # Redundancy Score
-            r_score = 0
+    def _compute_r_score(self, subset_indices, alpha=0.2):
+        """
+        Computes Redundancy Score: The point should be distant from all the other elements in the subset.
+        :param subset_indices:
+        :param alpha:
+        :return:
+        """
+        r_score = 0
+        for index in subset_indices:
+            p_act = self.penultimate_activations[index]
             if len(subset_indices) > 1:
                 subset_penultimate_acts = [self.penultimate_activations[i] for i in subset_indices]
                 dist = np.linalg.norm(subset_penultimate_acts - p_act, axis=1)
-                r_score = np.min(dist[dist != 0])
+                score = np.min(dist[dist != 0])
+                r_score += alpha * score
+        return r_score
 
-            score += d_score + alpha_1*u_score + alpha_2*r_score
-        return score
+    def compute_md_score(self, subset_indices, class_mean, alpha=2.):
+        """
+        Computes Mean Divergence score: The new datapoint should be close to the class mean
+        :param subset_indices:
+        :param class_mean:
+        :param alpha:
+        :return:
+        """
+        md_score = 0
+        for index in subset_indices:
+            p_act = self.penultimate_activations[index]
+            score = np.linalg.norm(class_mean-p_act, axis=0)
+            md_score += alpha * score
+        return md_score
