882a10d824e5c39a23eff76d97e8b6dda8b2f6ce
vamshiteja <rachavamshiteja@gmail.com>
2018-11-15 17:04:11 +0530

    Vectorised SubModularity

--- lib/samplers/submodular.py
+++ lib/samplers/submodular.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import copy
 from multiprocessing.pool import ThreadPool
+from operator import itemgetter
 
 import torch
 import torch.nn.functional as F
@@ -12,17 +13,18 @@ from lib.utils import log
 
 
 class SubModSampler(Sampler):
-    def __init__(self, model, dataset, batch_size):
+    def __init__(self, model, dataset, batch_size, r_size=1024):
         super(SubModSampler, self).__init__(model, dataset)
         self.batch_size = batch_size
         self.index_set = range(0, len(self.dataset))    # It contains the indices of each image of the set.
+        self.r_size = r_size
 
     def get_subset(self):
 
         set_size = len(self.index_set)
+        num_of_partitions = cfg.num_of_partitions
 
-        if set_size >= set_size*self.batch_size:
-            num_of_partitions = cfg.num_of_partitions
+        if set_size >= num_of_partitions*self.batch_size:
             size_of_each_part = set_size / num_of_partitions
             partitions = [self.index_set[k:k+size_of_each_part] for k in range(0, set_size, size_of_each_part)]
 
@@ -30,7 +32,7 @@ class SubModSampler(Sampler):
             pool_handlers = []
             for partition in partitions:
                 handler = pool.apply_async(get_subset_indices, args=(partition, self.penultimate_activations, self.final_activations,
-                                                self.batch_size))
+                                                self.batch_size, self.r_size))
                 pool_handlers.append(handler)
             pool.close()
             pool.join()
@@ -45,7 +47,7 @@ class SubModSampler(Sampler):
         log(intermediate_indices)
 
         subset_indices = get_subset_indices(intermediate_indices, self.penultimate_activations, self.final_activations,
-                                            self.batch_size)
+                                            self.batch_size, self.r_size)
 
         for item in subset_indices:     # Subset selection without replacement.
             self.index_set.remove(item)
@@ -54,8 +56,9 @@ class SubModSampler(Sampler):
         return subset_indices
 
 
-def get_subset_indices(index_set_input, penultimate_activations, final_activations, subset_size, alpha_1=1, alpha_2=1, alpha_3=1):
+def get_subset_indices(index_set_input, penultimate_activations, final_activations, subset_size, r_size, alpha_1=1, alpha_2=1, alpha_3=1):
 
+    #index_set = np.random.choice(index_set_input,r_size,replace=True)
     index_set = copy.deepcopy(index_set_input)
     subset_indices = []     # Subset of indices. Keeping track to improve computational performance.
 
@@ -74,8 +77,16 @@ def get_subset_indices(index_set_input, penultimate_activations, final_activatio
         # u_score = compute_u_score(list(subset_indices))
 
         # Same logic for md_score
-        md_score = compute_md_score(penultimate_activations, list(subset_indices), class_mean)
+        md_score = np.sum(compute_md_score(penultimate_activations, list(subset_indices), class_mean))
 
+        md_scores = md_score+compute_md_score(penultimate_activations, list(index_set), class_mean)
+        #u_score = u_score + compute_u_score(final_activations, list([index_set))
+        #d_score = d_score + compute_d_score(penultimate_activations, list([index_set))
+
+        #r_score = compute_r_score(penultimate_activations, list(temp_subset_indices))
+        scores = md_scores
+
+        '''
         for iter, item in enumerate(index_set):
             temp_subset_indices = list(subset_indices)
             temp_subset_indices.append(index_set[iter])
@@ -88,12 +99,13 @@ def get_subset_indices(index_set_input, penultimate_activations, final_activatio
             #score = d_score + u_score + r_score + md_score
             score = md_score
             scores.append(score)
+        '''
 
         best_item_index = np.argmax(scores)
         subset_indices.append(index_set[best_item_index])
         index_set = np.delete(index_set, best_item_index, axis=0)
 
-        log('Processed: {0}/{1} exemplars. Time taken is {2} sec.'.format(i, subset_size, time.time()-now))
+        #log('Processed: {0}/{1} exemplars. Time taken is {2} sec.'.format(i, subset_size, time.time()-now))
 
     return subset_indices
 
@@ -159,11 +171,22 @@ def compute_md_score(penultimate_activations, subset_indices, class_mean, alpha=
     :param subset_indices:
     :param class_mean:
     :param alpha:
-    :return:
+    :return: list of scores for each subset item
     """
-    md_score = 0
+    if(len(subset_indices)==0):
+        return 0
+    elif(len(subset_indices)==1):
+        return np.sqrt(np.sum(np.square(penultimate_activations[subset_indices[0]]-class_mean)))
+    else:
+        pen_act = np.array(itemgetter(*subset_indices)(penultimate_activations))-np.array(class_mean)
+        md_score = alpha*np.sqrt(np.sum(np.square(pen_act),axis=1))
+        #md_score = np.sum(np.square((pen_act)))
+        #print md_score
+        return md_score
+
+    '''md_score = 0
     for index in subset_indices:
         p_act = penultimate_activations[index]
         score = np.linalg.norm(class_mean-p_act, axis=0)
         md_score += alpha * score
-    return md_score
+    return md_score'''
