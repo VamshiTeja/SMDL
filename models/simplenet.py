d0cab53b0e3249cc8b627d1277b85f4fcd6e8b77
Joseph KJ <josephkj20@gmail.com>
2018-11-18 15:58:34 +0530

    MNIST

--- models/simplenet.py
+++ models/simplenet.py
@@ -12,8 +12,8 @@ class SimpleNet(nn.Module):
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 25)
-        self.fc2 = nn.Linear(25, 10)
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
